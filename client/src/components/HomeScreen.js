7110a5d1c55a9a8885108892ff3090a12530c0c4
Neetu Agarwal <agarwalneetu301@gmail.com>
2021-04-09 23:59:45 +0530

    make homescreen and navbar

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import Video from './Video';
+import video from '../assets/video.mp4';
+import {Button} from 'react-bootstrap';
 
-const HomeScreen = () => {
+const videoStyle = {
+    position :"relative",
+    height :"30rem",
+    width :"100%",
+    backgroundColor : "black",
+    position : "fixed",
+    marginBottom : "0"
+}
+
+const heading = {
+    marginTop :"40px",
+    textAlign:"center",
+    color : "#8a165f"
+}
+
+
+const tagline = {
+    textAlign:"center"
+}
+
+const overText = {
+    color : "#b5d46e",
+    position: "absolute",
+        zIndex: "999",
+        margin: "0 auto",
+        left: "0",
+        right: "0",
+        top: "85%",
+        textAlign : "center"
+}
+
+
+
+const App = () => {
     return (
-        <div>
-            Home
+        <div >
+
+            <h1 my-2 style= {heading}>Welcome to ClubMix!</h1>
+            <h5 style = {tagline}>Discover the Possibilities with US!</h5>
+           <Video style = {videoStyle} source={video}/>
+            <h4 style={overText}>MNNIT, One Conservatory, One Community, Infinite Possibilities</h4>
         </div>
     )
 }
 
-export default HomeScreen
+export default App
